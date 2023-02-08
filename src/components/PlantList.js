e09e2f17723c77ff22727598451b344b1f48bd88
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-02-08 10:24:26 -0500

    plant challenge with all core deliverables

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,23 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plantList, search}) {
+
+  const filteredList = plantList.filter(plant => {
+    return plant.name.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{
+      filteredList.map(plant => {
+        return <PlantCard
+        key={plant.id}
+        name={plant.name}
+        image={plant.image}
+        price={plant.price}
+        />
+      })
+    }</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plantList, setPlantList] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch(`http://localhost:6001/plants`)
+      .then(response => response.json())
+      .then(data => setPlantList(data))
+  }, [])
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm
+      setPlantList={setPlantList}
+      />
+      <Search 
+      search={search}
+      setSearch={setSearch}
+      />
+      <PlantList 
+      plantList={plantList}
+      setPlantList={setPlantList}
+      search={search}
+      />
     </main>
   );
 }
